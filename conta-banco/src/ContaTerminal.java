4538b1b6dee007c8b65f2c14524800983842a1f2
mindmt <166029307+mindmt@users.noreply.github.com>
2024-04-09 15:40:20 -0300

    Add files via upload

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+import java.util.Locale; 
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US); // Utilize o Locale.US para forçar o uso do ponto como separador decimal
+
+        System.out.println("Por favor, digite o número da Agência");
+        int agencia = sc.nextInt();
+
+        sc.nextLine();
+
+        System.out.println("Por favor, digite o número da conta");
+        String numero = sc.nextLine();
+
+        System.out.println("Por favor, digite seu nome");
+        String nome = sc.nextLine();
+
+        System.out.println("Por favor, insira seu saldo");
+        double saldo = sc.nextDouble();
+
+        sc.close();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
     }
 }
